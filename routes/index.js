a0ad2857224e8eb2f9281dbb3433fe518534f225
rohit raout <rohitr204btechcse2024@kccitm.edu.in>
2022-06-01 00:47:14 +0530

    ReqnRes comment1

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ router.get('/', function(req, res, next) {
   console.log(req);//print the req
   res.json({name:"Saurav"});//send the response to the server basically a (Get request) 
   // res.render('index', { title: 'Express' });
+  //hehehe cheking for push
 });
 
 module.exports = router;
