ba2e28f1854e33a852792958ab373f2c6cf6263a
sunny <guptasunny3905@gmail.com>
2022-06-10 04:30:34 -0400

    new

--- routes/login.js
+++ routes/login.js
@@ -10,8 +10,8 @@ var mysql = require('mysql'); //calling another file or exporting
   router.get('/', function(req, res) {
     modelLogin.a(modelLogin.pandy.name)
     res.render('login');
-  });
-
+  } );
+ 
 
   var con = mysql.createConnection({
     host: "192.168.17.44",
